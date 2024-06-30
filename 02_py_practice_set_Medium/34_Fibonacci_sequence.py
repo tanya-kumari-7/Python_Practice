8d56732112011e35934cb8463e7d0247b1967a1e
sonamtanya1996 <sonamtanya1996@gmail.com>
2024-06-30 22:44:23 +0530

    Fibonacci_sequence2

--- 02_py_practice_set_Medium/34_Fibonacci_sequence.py
+++ 02_py_practice_set_Medium/34_Fibonacci_sequence.py
@@ -9,6 +9,7 @@ Created on Sun Jun 30 22:12:56 2024
 # Create a Fibonacci sequence
 # =============================================================================
 
+''' Method one '''
 def Fibonacci_sequence(num):
     Fibonacci_list=[1,0]
     for x in range(1,num-1):
@@ -18,4 +19,30 @@ def Fibonacci_sequence(num):
 
 result = Fibonacci_sequence(15)
 print(result)
+
+'''Method two'''
+
+def Fibonacci_sequence2(num):
+    Fibonacci_list2=[]
+    a=0
+    b=1
+    for x in range(num):
+        if len(Fibonacci_list2)==0:
+            Fibonacci_list2.append(a)
+        else:
+            c=a+b
+            b=a
+            a=c
+            Fibonacci_list2.append(c)
+    
+    return Fibonacci_list2
+
+result = Fibonacci_sequence2(10)
+print(result)
+
+
+        
+    
+  
+    
     
