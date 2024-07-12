3997dca83dad64e18ceffdc9d436f4776fe0385a
sonamtanya1996 <sonamtanya1996@gmail.com>
2024-07-12 22:55:57 +0530

    29_droping_row_from_DF

---
+++ 04_Pandas_dataframe/29_droping_row_from_DF.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 11 21:27:41 2024
+
+@author: user
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 10 20:47:47 2024
+
+@author: user
+"""
+
+# =============================================================================
+'''
+Write a Pandas program to delete DataFrame row(s) 
+based on given column value.
+'''
+# =============================================================================
+import pandas as pd
+import numpy as np
+
+exam_data = {
+    'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+    'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+    'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+    'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df=pd.DataFrame(exam_data,index=labels)
+print(df)
+
+df.drop('j',inplace=True)
