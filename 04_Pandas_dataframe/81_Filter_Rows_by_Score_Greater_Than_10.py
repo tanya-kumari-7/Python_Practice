5e84bd94c84e0e7e1406a4b1f338cab1afd9ee4e
sonamtanya1996 <sonamtanya1996@gmail.com>
2024-07-17 20:23:14 +0530

    81_Filter_Rows_by_Score_Greater_Than_10

---
+++ 04_Pandas_dataframe/81_Filter_Rows_by_Score_Greater_Than_10.py
@@ -0,0 +1,27 @@
+# =============================================================================
+'''
+Basic Filtering:
+Sample Data: A DataFrame df with columns.
+Question: Filter rows where values in score is greater than 10.
+''' 
+# =============================================================================
+
+import pandas as pd
+import numpy as np
+
+exam_data = {
+    'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+    'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+    'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+    'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df=pd.DataFrame(exam_data,index=labels)
+print(df)
+
+df.info()
+df.fillna(0,inplace=True)
+df['score']=df['score'].astype(int)
+
+df[df['score'] >= 10]
+    
