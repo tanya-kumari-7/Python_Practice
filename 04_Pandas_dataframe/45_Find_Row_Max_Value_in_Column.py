02d5a4582d1acf358159bb0f8b843eebd00da093
sonamtanya1996 <sonamtanya1996@gmail.com>
2024-07-17 18:35:37 +0530

    45_Find_Row_Max_Value_in_Column

---
+++ 04_Pandas_dataframe/45_Find_Row_Max_Value_in_Column.py
@@ -0,0 +1,24 @@
+# =============================================================================
+'''
+Write a Pandas program to find the row for where the value of a 
+given column is maximum.
+''' 
+# =============================================================================
+
+import pandas as pd
+import numpy as np
+
+exam_data = {
+    'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+    'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+    'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+    'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df=pd.DataFrame(exam_data,index=labels)
+print(df)
+
+max_score=df[df['score'] == df['score'].max()]
+print(max_score)
+  
+
