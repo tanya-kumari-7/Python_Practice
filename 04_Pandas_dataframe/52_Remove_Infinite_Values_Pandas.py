6b75b6b91016833df8e34703f6ebdb039bbbfb95
sonamtanya1996 <sonamtanya1996@gmail.com>
2024-07-23 21:40:04 +0530

    52_Remove_Infinite_Values_Pandas

---
+++ 04_Pandas_dataframe/52_Remove_Infinite_Values_Pandas.py
@@ -0,0 +1,20 @@
+
+# =============================================================================
+'''
+Write a Pandas program to remove infinite values from a given DataFrame.
+'''
+# =============================================================================
+import pandas as pd
+import numpy as np
+
+exam_data = {
+    'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+    'score': [12.5, 9, 16.5, np.inf, 9, 20, 14.5, -np.inf, 8, 19],
+    'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+    'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df=pd.DataFrame(exam_data,index=labels)
+print(df)
+
+df.replace([np.inf,-np.inf],np.nan,inplace=True)
