38067eba9ad3113d376aea344ae4126532074f6d
sonamtanya1996 <sonamtanya1996@gmail.com>
2024-07-23 22:09:56 +0530

    53_Insert_Column_at_Index_Pandas

---
+++ 04_Pandas_dataframe/53_Insert_Column_at_Index_Pandas.py
@@ -0,0 +1,25 @@
+
+# =============================================================================
+'''
+Write a Pandas program to insert a given column at a 
+specific column index in a DataFrame.
+'''
+# =============================================================================
+import pandas as pd
+import numpy as np
+
+exam_data = {
+    'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+    'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+    'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+    'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df=pd.DataFrame(exam_data,index=labels)
+print(df)
+
+# Adding column at the end of df (as a last column)
+df['rating']=[1,2,3,4,5,6,7,8,9,10]
+
+# Adding column at the end of df (as a last column)
+df.insert(loc=3, column='rating', value=[1,2,3,4,5,6,7,8,9,10])
