33a2f2b9760d9630ac6e7fa5ff29a79ba170b887
sonamtanya1996 <sonamtanya1996@gmail.com>
2024-08-05 21:11:34 +0530

    7_Group_by_Customer_ID_Purchase_Stats_Pandas

---
+++ 04_Pandas_dataframe/02_Pandas_grouping_aggregating/7_Group_by_Customer_ID_Purchase_Stats_Pandas.py
@@ -0,0 +1,20 @@
+# =============================================================================
+'''
+Write a  Pandas program to split a dataset, 
+group by one column and get mean, min, and max values by group.
+Using the following dataset find the mean, min, and max values of 
+purchase amount (purch_amt) group by customer id (customer_id).
+
+'''
+# =============================================================================
+import pandas as pd
+
+orders_data = pd.DataFrame({
+'ord_no':[70001,70009,70002,70004,70007,70005,70008,70010,70003,70012,70011,70013],
+'purch_amt':[150.5,270.65,65.26,110.5,948.5,2400.6,5760,1983.43,2480.4,250.45, 75.29,3045.6],
+'ord_date': ['2012-10-05','2012-09-10','2012-10-05','2012-08-17','2012-09-10','2012-07-27','2012-09-10','2012-10-10','2012-10-10','2012-06-27','2012-08-17','2012-04-25'],
+'customer_id':[3005,3001,3002,3009,3005,3007,3002,3004,3009,3008,3003,3002],
+'salesman_id': [5002,5005,5001,5003,5002,5001,5001,5006,5003,5002,5007,5001]
+})
+
+result=orders_data.groupby('customer_id').agg({'purch_amt':['mean','min','max']})
