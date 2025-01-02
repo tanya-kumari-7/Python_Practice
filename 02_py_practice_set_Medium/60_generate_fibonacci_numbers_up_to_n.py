673d8e474778a833e0662907a4616b28e897acd8
sonamtanya1996 <sonamtanya1996@gmail.com>
2025-01-02 23:11:57 +0530

    60_generate_fibonacci_numbers_up_to_n

--- 02_py_practice_set_Medium/60_generate_fibonacci_numbers_up_to_n.py
+++ 02_py_practice_set_Medium/60_generate_fibonacci_numbers_up_to_n.py
@@ -1,14 +1,11 @@
-#Generate Fibonacci numbers up to n.
-
 def Fibonacci_func(n):
-    Fibonacci_list = [0,1]
+    Fibonacci_list = [0, 1]
     num = 1
-    while num <= n:
+    while num +Fibonacci_list[-2] <= n:  # Include numbers that are less than or equal to n
         new_num = Fibonacci_list[-1] + Fibonacci_list[-2]
         Fibonacci_list.append(new_num)
         num = new_num
     return Fibonacci_list
-    
 
 output = Fibonacci_func(10)
 print(output)
