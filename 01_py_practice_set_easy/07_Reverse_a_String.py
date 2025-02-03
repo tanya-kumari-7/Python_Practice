671b49f3e2999f647a8def374816720964fda611
sonamtanya1996 <sonamtanya1996@gmail.com>
2025-02-03 20:52:43 +0530

    Reverse a String

---
+++ 01_py_practice_set_easy/07_Reverse_a_String.py
@@ -0,0 +1,12 @@
+'''
+Reverse a String: Write a function to reverse a given 
+string
+'''
+
+str_ = 'gaurav'
+
+def reverse_func (string):
+    return string[::-1]
+
+result = reverse_func(str_)
+print(result)
