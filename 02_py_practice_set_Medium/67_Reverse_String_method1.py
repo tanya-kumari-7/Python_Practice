afbf3349477efd6422bbd79f13674bde956f4ccb
sonamtanya1996 <sonamtanya1996@gmail.com>
2025-02-10 22:02:09 +0530

    67_Reverse_String_method1

---
+++ 02_py_practice_set_Medium/67_Reverse_String_method1.py
@@ -0,0 +1,16 @@
+'''
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+'''
+
+string = ["H","a","n","n","a","h"]
+
+ 
+def Reverse_String_method1 (price):
+    Reverse_String = list(reversed(string))
+    return (Reverse_String)
+
+Function_result = Reverse_String_method1(string)
+print(Function_result)
+
