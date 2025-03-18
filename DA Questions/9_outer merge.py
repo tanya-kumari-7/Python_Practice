2bcf84fb5f9b3d005190425f7bd8534ba245c482
sonamtanya1996 <sonamtanya1996@gmail.com>
2025-03-18 23:49:26 +0530

    9_outer merge

---
+++ DA Questions/9_outer merge.py
@@ -0,0 +1,35 @@
+''' Q4. What is the difference between merge(), join(), and concat() in Pandas?
+A:
+
+merge() – Combines DataFrames based on keys
+join() – Combines DataFrames based on the index
+concat() – Stacks DataFrames vertically or horizonta 
+
+'''
+
+import pandas as pd
+import numpy as np
+
+# merge
+df1 = pd.DataFrame({'A': [1, 2], 'B': [3, 4]})
+df2 = pd.DataFrame({'A': [4, 2], 'C': [5, 6]})
+                                       
+mergedf = df1.merge(df2, on= 'A', how= 'inner')                                      
+mergedf
+mergedf2 = pd.merge(df1,df2,on='A',how = 'inner')
+mergedf2
+
+mergedf = df1.merge(df2, on= 'A', how= 'left')                                      
+mergedf
+mergedf2 = pd.merge(df1,df2,on='A',how = 'left')
+mergedf2
+
+mergedf = df1.merge(df2, on= 'A', how= 'right')                                      
+mergedf
+mergedf2 = pd.merge(df1,df2,on='A',how = 'right')
+mergedf2
+
+mergedf = df1.merge(df2, on= 'A', how= 'outer')                                      
+mergedf
+mergedf2 = pd.merge(df1,df2,on='A',how = 'outer')
+mergedf2
