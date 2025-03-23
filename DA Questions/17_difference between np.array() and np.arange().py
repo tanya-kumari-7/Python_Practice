23fc37d38d26dbbf0a2484c15ed8dd46129ad90e
sonamtanya1996 <sonamtanya1996@gmail.com>
2025-03-23 22:52:05 +0530

    17_difference between np.array() and np.arange()

---
+++ DA Questions/17_difference between np.array() and np.arange().py
@@ -0,0 +1,11 @@
+''' Q6. What is the difference between np.array() and np.arange()? '''
+
+import pandas as pd
+import numpy as np
+
+arr = np.array([1,2,3,4,5])
+print(arr)
+type(arr)
+
+arange = np.arange(1,11,3)
+print(arange)
