13c2bed3ce35e20ea9d34d028c8cc24c6a513655
sonamtanya1996 <sonamtanya1996@gmail.com>
2025-03-24 20:43:10 +0530

    18_reshape_array

--- DA Questions/18_reshape_array.py
+++ DA Questions/18_reshape_array.py
@@ -3,9 +3,12 @@
 import pandas as pd
 import numpy as np
 
-arr = np.array([1,2,3,4,5])
+arr = np.array([1,2,3,4,5,6])
 print(arr)
 type(arr)
 
-reshaped = arr.reshape(1,3)
+reshaped = arr.reshape(3,2)
+print(reshaped)
+
+reshaped = arr.reshape(2,3)
 print(reshaped)
