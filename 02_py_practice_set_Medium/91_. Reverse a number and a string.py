3e264d5d100e2dc7a28f9b67570f2654c42aed82
sonamtanya1996 <sonamtanya1996@gmail.com>
2025-05-05 22:08:59 +0530

    set 1

--- 02_py_practice_set_Medium/91_. Reverse a number and a string.py
+++ 02_py_practice_set_Medium/91_. Reverse a number and a string.py
@@ -1,12 +1,31 @@
-str = 'sonam'
-num = 123458976
+del str
 
+
+name = 'sonam'
+num = 123458976
 
 # Function to reverse a string
 def reverse_string(s):
-    x = str[::-1]
-    return x
+    return s[::-1]
 
-result = reverse_string(str)
-print(result)
+result = reverse_string(name)
+print("Reversed name:", result)
 
+# Function to reverse a number
+def reverse_number(n):
+    return str(n)[::-1]  # Convert to string and reverse it
+
+num_reversed = reverse_number(num)
+print("Reversed number (as string):", num_reversed)
+
+# Function to reverse a number using loop
+
+
+############ Method 1 
+
+num = 123467744
+for x in str(num):
+    print(str(num)[::-1])
+    break
+
+############# method 2 
