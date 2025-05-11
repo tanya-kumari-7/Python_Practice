dcf3de8be21a5f27567acfb7de7036d048e9ffbf
Tanya Kumari <sonamtanya1996@gmail.com>
2025-05-11 21:19:29 +0530

    Data Analysis

---
+++ Data_Analysis_using_pandas_03/DATAANALYSIS.py
@@ -0,0 +1,57 @@
+# IMPORTS
+import pandas as pd
+import plotly.express as px
+
+# create a DataFrame
+
+import pandas as pd
+import numpy as np
+
+
+data = {
+    "Order_ID": [1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10],  # Duplicating Order_IDs 1 and 10
+    "Customer_Name": ['Alice', 'Alice', 'Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'Grace', 'Hannah', 'Ivan', 'Ivan'],
+    "Product": ['Laptop', 'Laptop', 'Laptop', 'Laptop', 'Phone', 'Tablet', 'Tablet', 'Laptop', 'Phone', 'Tablet', 'Laptop', 'Laptop'],
+    "Category": ['Electronics'] * 12,
+    "Order_Amount": [1200, 1200, 1200, 1300, 800, 600, 650, 1150, 900, 700, 1400, 1400],
+    "Discount": [100, 100, 100, 150, 50, 20, 30, 80, 60, 40, 200, 200],
+    "Order_Date": pd.date_range(start='2024-01-01', periods=12, freq='W'),
+    "Region": ['North', 'North', 'North', 'South', 'East', 'North', 'South', 'East', 'North', 'West', 'West', 'West']
+}
+df = pd.DataFrame(data)
+print(df)
+
+# Exploring the DataFrame
+
+# basic information about the DataFrame
+df.shape
+df.head()
+df.tail()
+df.count()
+df.describe()
+df.info()
+len(df)
+df.columns
+df.dtypes
+df.isnull().sum()
+
+# Checking for duplicates
+df.duplicated().sum()  # Check for duplicates
+
+for col in df.columns:
+    total_value = df[col].shape[0]
+    unique_value = df[col].nunique()
+    duplicate_count = total_value - unique_value
+    print(f"Column '{col}' has {duplicate_count} duplicates.")
+
+
+
+
+# Exploring data from the DataFrame
+df['Product'].unique()
+df['Product'].nunique()
+df['Product'].value_counts()
+df['Product'].value_counts(normalize=True)
+df['Product'].value_counts().plot(kind='bar')
+
+
