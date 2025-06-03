7e2b20cbf30426fd3bc21b27aa914f2b6b65c851
Tanya Kumari <sonamtanya1996@gmail.com>
2025-06-03 17:24:30 +0530

    python problem

---
+++ 02_py_practice_set_Medium/93_function_to_reverse_a_string.py
@@ -0,0 +1,13 @@
+'''
+Write a function to reverse a string without using built-in functions.
+
+'''
+
+
+string = 'vishal'
+
+def reverse_string_func(string):
+    return string[::-1]
+
+result = reverse_string_func(string)
+print(result)
