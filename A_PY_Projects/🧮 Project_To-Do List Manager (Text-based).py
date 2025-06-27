2a06617b800e631f55490649319dda15438602ba
Tanya Kumari <sonamtanya1996@gmail.com>
2025-06-27 21:03:52 +0530

    unsupervised learning

--- A_PY_Projects/🧮 Project_To-Do List Manager (Text-based).py
+++ A_PY_Projects/🧮 Project_To-Do List Manager (Text-based).py
@@ -1,8 +1,10 @@
 """ Creating a function to add task in a file """
 
+def add_task(task):
+    if not task.strip():
+        return "❌ Task cannot be empty."
 
-
-
-
-
+    with open("tasks.txt", "a") as file:
+        file.write(task + " | Not Done\n")
+    return "✅ Task added!"
 
