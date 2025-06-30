6a6a1e3c98c0e43f034030baf412f2bf94458aa9
Tanya Kumari <sonamtanya1996@gmail.com>
2025-06-30 22:35:45 +0530

    Project_book_managar

--- A_PY_Projects/BookManager.py
+++ A_PY_Projects/BookManager.py
@@ -7,3 +7,16 @@ def add_book_func(title,author,status):
         return "Book details added to BookTracker"
 
 
+def readbook():
+    try:
+        with open("booktracker.txt", "r") as file:
+            book_details = file.readlines()
+            if not book_details:
+                print("📭 No brook detail found.")
+            else:
+                for i,lines in enumerate(book_details,start=1):
+                    title,author,status =  lines.strip().split("|")
+                    print(f"{i}.title: {title.strip()} , author{author.strip()}, status: {status.strip()}")
+                    
+    except FileNotFoundError:
+        print("⚠️ File not found. Add a note first.") 
