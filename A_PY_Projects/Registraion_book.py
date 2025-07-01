4d07672ba9f7aad94b83a9bbd7dd77511be9a257
Tanya Kumari <sonamtanya1996@gmail.com>
2025-07-01 22:19:04 +0530

    Registration

--- A_PY_Projects/Registraion_book.py
+++ A_PY_Projects/Registraion_book.py
@@ -7,3 +7,16 @@ def registration(name,email,password,registration_date):
         with open('registrationbook.txt','a') as file:
             file.write(f'{name} | {email} | {password}| {registration_date}\n')
         return "Registrion successfuly completed"
+    
+def read_registration_book():
+    try:
+        with open("registrationbook",'r') as file:
+            user_details = file.readlines()
+            if not user_details:
+                return (f' User details not Found')
+            for i,line in enumerate(user_details,start=1):
+                name,email,password,registration_date = line.strip().split("|")
+                return (f'{i}. name: {name}, email: {email}, password: {password}, registration_date:{registration_date}')
+    except Exception as r:
+        print(f'File not found, got error as {r}')
+        
