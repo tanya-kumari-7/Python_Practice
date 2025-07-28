2df35efbde47d1c41942fe1805d0619de2426f60
Tanya Kumari <87415938+tanya-kumari-7@users.noreply.github.com>
2025-07-28 14:33:01 +0530

    Py project

---
+++ 02_py_practice_set_Medium/String_problems.py
@@ -0,0 +1,14 @@
+'''✅ String Problems '''
+
+## Reverse a string
+
+string = 'sonam'
+
+def func_reverse_string(x):
+    retrun x[::-1]
+
+func_call = func_reverse_string(string)
+print(func_call)
+
+    
+    
