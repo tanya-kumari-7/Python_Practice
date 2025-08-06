849986761890afa4b41d0f3997ba9ee009a86506
Tanya Kumari <87415938+tanya-kumari-7@users.noreply.github.com>
2025-08-06 15:25:48 +0530

    Py problems

--- 02_py_practice_set_Medium/String_problems.py
+++ 02_py_practice_set_Medium/String_problems.py
@@ -120,3 +120,20 @@ def compress_string_func(x):
 
 print(compress_string_func('sssoonnaammm'))
     
+
+## Compress a string (with run-length encoding)
+
+
+def compress_string_func(x):
+    dic_box = ''
+    count = 1
+    for i  in range(1,len(x)) :
+        if x[i] == x[i-1]:
+            count += 1
+        else:
+            dic_box += x[i-1] + str(count)
+            count = 1
+    dic_box += x[-1] + str(count)
+    return dic_box
+
+print(compress_string_func('sssoonnaammm'))
--- 02_py_practice_set_Medium/tempCodeRunnerFile.py
+++ 02_py_practice_set_Medium/tempCodeRunnerFile.py
@@ -1,17 +1,17 @@
-## Compress a string (without run-length encoding)
+## Compress a string (with run-length encoding)
 
 
 def compress_string_func(x):
-    dic_box = {}
-    for i  in x :
-        if i not in dic_box:
-            dic_box[i] = 1
-        else :
-            dic_box[i] += 1
-    result = ''
-    for a,b in dic_box.items():
-        result += a+str(b)
-
-    return result
+    dic_box = ''
+    count = 1
+    for i  in range(1,len(x)) :
+        if x[i] == x[i-1]:
+            count += 1
+        else:
+            dic_box += x[i-1] + str(count)
+            count = 1
+    dic_box += x[-1] + str(count)
+    return dic_box
 
 print(compress_string_func('sssoonnaammm'))
+    
