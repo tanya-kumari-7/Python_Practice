de6396a4461bc2137799b333447ea332f38a73e6
Tanya Kumari <87415938+tanya-kumari-7@users.noreply.github.com>
2025-08-06 16:14:12 +0530

    Py problems

---
+++ A_PY_Projects/Guessing_Game_Project.py
@@ -0,0 +1,36 @@
+import random
+
+def number_guessing_game():
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100...")
+
+
+    number_to_guess = random.randint(1, 1000)
+    attempts = 0
+    Game_number = []
+    user_number = []
+    Game_number.append(number_to_guess)
+
+    while True:
+        guess = input("Enter your guess: ")
+        user_number.append(guess)
+
+        # check if input is a number
+        if not guess.isdigit():
+            print("Please enter a valid number")
+            continue
+
+        guess = int(guess)
+        attempts += 1
+
+        if guess < number_to_guess:
+            print("Too low! Try again.")
+        elif guess > number_to_guess:
+            print("Too high! Try again.")
+        else:
+            print(f"Correct! You guessed the number in {attempts} tries.")
+            print(f"{Game_number} Game Number")
+            print(f"{user_number} The list of Your Number")
+            break
+
+number_guessing_game()
