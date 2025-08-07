6b56bcc0903540a7108a7c7629d9d618cf76d86c
Tanya Kumari <87415938+tanya-kumari-7@users.noreply.github.com>
2025-08-07 16:21:13 +0530

    Py problems

--- A_PY_Projects/tempCodeRunnerFile.py
+++ A_PY_Projects/tempCodeRunnerFile.py
@@ -1,43 +1,13 @@
-import random
+def show_menu():
+    print("Welcome to Task Manager App")
 
-def number_guessing_game():
-    print("Welcome to the Number Guessing Game!")
-    print("I'm thinking of a number between 1 and 100...")
+    print("Hope doing Great!") 
 
+    print("Here is your menu")
+    print(" 1. View Tasks")
+    print(" 2. Add Task")
+    print(" 3. Mark Task as Done")
+    print(" 4. Delete Task")
+    print(" 5. Exit")
 
-    number_to_guess = random.randint(1, 100)
-    attempts = 0
-    Game_number = []
-    user_number = []
-    Game_number.append(number_to_guess)
-
-    while True:
-        guess = input("Enter your guess: ")
-        user_number.append(guess)
-
-        # check if input is a number
-        if not guess.isdigit():
-            print("Please enter a valid number")
-            continue
-        if int(guess) > 100:
-            print("Please enter number between 1 to 100")
-            continue
-
-
-        guess = int(guess)
-        attempts += 1
-
-        if guess < number_to_guess:
-            print("Too low! Try again.")
-        elif guess > number_to_guess:
-            print("Too high! Try again.")
-        else:
-            print(f"Correct! You guessed the number in {attempts} tries.")
-            print(f"{Game_number} Game Number")
-            print(f"{user_number} The list of Your Number")
-            break
-
-        if abs(guess - number_to_guess) <= 10:
-            print("You're very close! Within 10 numbers.")
-
-number_guessing_game()
+print(show_menu())
