f8361423df5781c3e041391fce458149cd86c00b
Tanya Kumari <87415938+tanya-kumari-7@users.noreply.github.com>
2025-09-02 16:33:15 +0530

    text to bq tbl

--- 02_py_practice_set_Medium/test.py
+++ 02_py_practice_set_Medium/test.py
@@ -3,11 +3,14 @@ from google.cloud import bigquery
 import pandas as pd
 from pandasql import sqldf
 
-file_path = r'C:\Users\user\Downloads\mydb_connection.json'
+file_path = r'C:\Users\Admin\Downloads\mydb_connection.json'
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = file_path
 
 client = bigquery.Client()
-query = "SELECT * FROM `bigquery-public-data.austin_crime.crime` LIMIT 10"
+query = "SELECT * FROM `mydb-470314.z_tanya.vle_master` LIMIT 5"
 df = client.query(query).to_dataframe()
 
 print(df.head())
+
+
+
