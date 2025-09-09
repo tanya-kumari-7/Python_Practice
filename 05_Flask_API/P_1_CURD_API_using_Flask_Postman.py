41daed55e43308a7cf7429f8ea982ef9dd51baee
Tanya Kumari <87415938+tanya-kumari-7@users.noreply.github.com>
2025-09-09 20:36:10 +0530

    P_1_CURD_API_using_Flask_Postman

--- 05_Flask_API/P_1_CURD_API_using_Flask_Postman.py
+++ 05_Flask_API/P_1_CURD_API_using_Flask_Postman.py
@@ -5,9 +5,10 @@ import pandas as pd
 app = Flask(__name__)
 client = bigquery.Client.from_service_account_json(r"C:/Users/Admin/Downloads.My workflow.json")
 
-PROJECT_ID = "your_project_id"
-DATASET_ID = "your_dataset"
-TABLE_ID = "your_table"
+# connecting to bigquery
+PROJECT_ID = "mydb-470314"
+DATASET_ID = "z_tanya"
+TABLE_ID = "vle_master"
 TABLE_REF = f"{PROJECT_ID}.{DATASET_ID}.{TABLE_ID}"
 
 
