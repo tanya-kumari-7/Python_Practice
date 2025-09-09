825d954282095d3d3adf3a4666dff106559b24bf
Tanya Kumari <87415938+tanya-kumari-7@users.noreply.github.com>
2025-09-09 20:31:01 +0530

    P_1_CURD_API_using_Flask_Postman

--- 05_Flask_API/P_1_CURD_API_using_Flask_Postman.py
+++ 05_Flask_API/P_1_CURD_API_using_Flask_Postman.py
@@ -1,1 +1,13 @@
 from flask import Flask,request,jsonify
+from google.cloud import bigquery
+import pandas as pd
+
+app = Flask(__name__)
+client = bigquery.Client.from_service_account_json(r"C:/Users/Admin/Downloads.My workflow.json")
+
+PROJECT_ID = "your_project_id"
+DATASET_ID = "your_dataset"
+TABLE_ID = "your_table"
+TABLE_REF = f"{PROJECT_ID}.{DATASET_ID}.{TABLE_ID}"
+
+
