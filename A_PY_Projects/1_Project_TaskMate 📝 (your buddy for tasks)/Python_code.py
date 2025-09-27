aa9c86d5500c5f7ab2ced7e21c421457f54b0745
Tanya Kumari <87415938+tanya-kumari-7@users.noreply.github.com>
2025-09-27 21:00:42 +0530

    Python Project

--- A_PY_Projects/1_Project_TaskMate 📝 (your buddy for tasks)/Python_code.py
+++ A_PY_Projects/1_Project_TaskMate 📝 (your buddy for tasks)/Python_code.py
@@ -18,26 +18,26 @@ def add_task(task_id,task_name,status,date):
             file.write(f"{task_id}|{task_name}|{status}|{date}\n")
             return "Task Added !! :)"
         
-add_task("1", "Update Trade Details", "Pending", "2025-01-01")
-add_task("2", "Check Payment Records", "In Progress", "2025-01-03")
-add_task("3", "Review Client Feedback", "Completed", "2025-01-05")
-add_task("4", "Prepare Monthly Report", "Pending", "2025-01-07")
-add_task("5", "Fix Dashboard Bugs", "In Progress", "2025-01-10")
-add_task("6", "Conduct Team Meeting", "Pending", "2025-01-12")
-add_task("7", "Update API Documentation", "Completed", "2025-01-15")
-add_task("8", "Test New Features", "Pending", "2025-01-18")
-add_task("9", "Analyze Market Data", "In Progress", "2025-01-20")
-add_task("10", "Optimize SQL Queries", "Pending", "2025-01-22")
-add_task("11", "Clean User Database", "Completed", "2025-01-25")
-add_task("12", "Design New Dashboard", "Pending", "2025-01-27")
-add_task("13", "Review Security Logs", "Pending", "2025-01-28")
-add_task("14", "Check Backup Files", "Completed", "2025-01-29")
-add_task("15", "Send Invoice Reminders", "In Progress", "2025-02-01")
-add_task("16", "Verify Tax Filings", "Pending", "2025-02-03")
-add_task("17", "Train New Employees", "Completed", "2025-02-05")
-add_task("18", "Prepare Presentation", "Pending", "2025-02-07")
-add_task("19", "Update User Permissions", "In Progress", "2025-02-10")
-add_task("20", "Plan Product Launch", "Pending", "2025-02-12")
+# add_task("1", "Update Trade Details", "Pending", "2025-01-01")
+# add_task("2", "Check Payment Records", "In Progress", "2025-01-03")
+# add_task("3", "Review Client Feedback", "Completed", "2025-01-05")
+# add_task("4", "Prepare Monthly Report", "Pending", "2025-01-07")
+# add_task("5", "Fix Dashboard Bugs", "In Progress", "2025-01-10")
+# add_task("6", "Conduct Team Meeting", "Pending", "2025-01-12")
+# add_task("7", "Update API Documentation", "Completed", "2025-01-15")
+# add_task("8", "Test New Features", "Pending", "2025-01-18")
+# add_task("9", "Analyze Market Data", "In Progress", "2025-01-20")
+# add_task("10", "Optimize SQL Queries", "Pending", "2025-01-22")
+# add_task("11", "Clean User Database", "Completed", "2025-01-25")
+# add_task("12", "Design New Dashboard", "Pending", "2025-01-27")
+# add_task("13", "Review Security Logs", "Pending", "2025-01-28")
+# add_task("14", "Check Backup Files", "Completed", "2025-01-29")
+# add_task("15", "Send Invoice Reminders", "In Progress", "2025-02-01")
+# add_task("16", "Verify Tax Filings", "Pending", "2025-02-03")
+# add_task("17", "Train New Employees", "Completed", "2025-02-05")
+# add_task("18", "Prepare Presentation", "Pending", "2025-02-07")
+# add_task("19", "Update User Permissions", "In Progress", "2025-02-10")
+# add_task("20", "Plan Product Launch", "Pending", "2025-02-12")
 
 
 def view_all_task():
@@ -45,9 +45,9 @@ def view_all_task():
         file_details = file.readlines()
 
         for line in file_details:
-            print(line.strip())  # removes \n so it looks clean
+            print(line.strip())  
    
-view_all_task()
+# view_all_task()
 
 def view_task_by_id(id):
     if not id:
@@ -68,7 +68,7 @@ def view_task_by_id(id):
         if not case_status:
             print(f"{input_id} : Id not found, please enter vaild id or please add task with this id [If ANY]")
 
-view_task_by_id(19)
+# view_task_by_id(19)
 
 
 
@@ -96,7 +96,7 @@ def delete_task_by_id(task_id):
         if not case_status:
             print(f"{input_task_id}, Not found in the book")
 
-delete_task_by_id(20)
+# delete_task_by_id(20)
 
 def update_status_date_by_id (id,updated_status,date):
     if not updated_status or not date or not id:
@@ -127,7 +127,7 @@ def update_status_date_by_id (id,updated_status,date):
             if not case_status:
                 print(f"{input_id} id not found in the book, please re-check")
 
-update_status_date_by_id(19,"Completed","2025-09-26")   
+# update_status_date_by_id(19,"Completed","2025-09-26")   
 
 
 def task_summary_by_status():
@@ -143,7 +143,7 @@ def task_summary_by_status():
             task_distribution = df.groupby("status")["task_id"].count()
             return task_distribution
 
-task_summary_by_status()
+# task_summary_by_status()
 
 def month_wise_task_summary():
     with open("text_book.txt","r") as file:
@@ -161,7 +161,7 @@ def month_wise_task_summary():
             month_wise_task_summary_ = df.groupby(["month_name","status"])["task_id"].count()
             return month_wise_task_summary_
         
-month_wise_task_summary()
+# month_wise_task_summary()
         
 
 
